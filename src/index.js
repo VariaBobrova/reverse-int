24f2387f44879108c8461d8f843d77be482513c3
varvara bobrova <varia.bobrova@gmail.com>
2021-12-29 20:39:15 +0200

    feat: reverse function added

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    let reversed = 0;
+    n = Math.abs(n);
+    function r(n) {
+        while (n) {
+            let len = (n.toString()).length;
+            reversed += (n % 10) * 10 ** (len - 1);
+            n = Math.floor(n / 10);
+            return r(n);
+        }
+    }
+    r(n);
+    return reversed;
 }
