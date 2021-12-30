2d83368ab30bd14b540c152572e6f7b21ee3b51d
varvara bobrova <varia.bobrova@gmail.com>
2021-12-30 11:22:54 +0200

    refactor: shorten function

--- src/index.js
+++ src/index.js
@@ -1,14 +1,10 @@
 module.exports = function reverse(n) {
     let reversed = 0;
     n = Math.abs(n);
-    function r(n) {
-        while (n) {
-            let len = (n.toString()).length;
-            reversed += (n % 10) * 10 ** (len - 1);
-            n = Math.floor(n / 10);
-            return r(n);
-        }
+    while (n) {
+        let len = (n.toString()).length;
+        reversed += (n % 10) * 10 ** (len - 1);
+        n = Math.floor(n / 10);    
     }
-    r(n);
     return reversed;
 }
